ed542febb3b96e19b7819d8f616f2ec9328cc0a7
p.karari <kararikelvin2002@gmail.com>
2024-06-28 16:04:30 +0300

    Book reducer.tsx

--- src/hooks/bookReducer.tsx
+++ src/hooks/bookReducer.tsx
@@ -1,25 +1,24 @@
 export type Book = {
-    id: number;
-    title: string;
-    author: string;
-    year: string;
-  };
-  
-  type Action =
-    | { type: 'add'; book: Book }
-    | { type: 'update'; book: Book }
-    | { type: 'delete'; id: number };
-  
-  export function bookReducer(state: Book[], action: Action): Book[] {
-    switch (action.type) {
-      case 'add':
-        return [...state, action.book];
-      case 'update':
-        return state.map((book) => (book.id === action.book.id ? action.book : book));
-      case 'delete':
-        return state.filter((book) => book.id !== action.id);
-      default:
-        return state;
-    }
+  id: number;
+  title: string;
+  author: string;
+  year: number;
+};
+
+type Action =
+  | { type: 'add'; book: Book }
+  | { type: 'update'; book: Book }
+  | { type: 'delete'; id: number };
+
+export function bookReducer(state: Book[], action: Action): Book[] {
+  switch (action.type) {
+    case 'add':
+      return [...state, action.book];
+    case 'update':
+      return state.map((book) => (book.id === action.book.id ? action.book : book));
+    case 'delete':
+      return state.filter((book) => book.id !== action.id);
+    default:
+      return state;
   }
-  
+}
